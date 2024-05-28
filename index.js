f60bab7cced9bc4f31eefbfbd2bbc00399205071
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2024-05-28 14:16:49 +0200

    streaming directly from youtube + automatic title change

--- index.js
+++ index.js
@@ -5,11 +5,49 @@ const fs = require('fs');
 const ytdl = require('ytdl-core');
 const { spawn } = require('child_process');
 const path = require('path');
+const util = require('util');
+const stat = util.promisify(fs.stat);
+const fsp = require('fs').promises;
+const { RefreshingAuthProvider } = require('@twurple/auth');
+const { ApiClient } = require('@twurple/api');
+
+
+const clientId = process.env.CLIENTID;
+const clientSecret = process.env.CLIENTSECRET;
+const botUserId = String(process.env.USERID);
+const channelUserId = String(process.env.CHANNELID);
+const botTokenData = JSON.parse(fs.readFileSync(`./token/tokens.${botUserId}.json`, 'UTF-8'));
+const channelTokenData = JSON.parse(fs.readFileSync(`./token/tokens.${channelUserId}.json`, 'UTF-8'));
+
+const authProvider = new RefreshingAuthProvider(
+	{
+		clientId,
+		clientSecret,
+		onRefresh: async (userId, newTokenData) => {
+			try {
+				fs.writeFileSync(`./token/tokens.${userId}.json`, JSON.stringify(newTokenData, null, 4), 'UTF-8')
+			} catch(ex) {
+				console.error(filename, ex);
+			}
+			
+		}
+	}
+);
+
+authProvider.addUser(botUserId, botTokenData, [
+    "chat"
+]);
+
+authProvider.addUser(channelUserId, channelTokenData, []);
+
+const twitchApi = new ApiClient({ authProvider });
 
 // RTMP-Server-URL aus Umgebungsvariablen lesen
 const rtmpUrl = process.env.RTMP_URL;
+const outputDir = 'data/';
 
 const fifoPath = path.join(__dirname, 'video_fifo');
+
 try {
   // Execute the shell command to create the FIFO file
   const result = spawnSync('mkfifo', [fifoPath]);
@@ -21,17 +59,43 @@ try {
   console.error('Fehler beim Erstellen der FIFO-Datei:', err);
 }
 
-
 // Playlist-Datei lesen
 function readPlaylist(file) {
   return fs.readFileSync(file, 'utf8').split('\n').filter(Boolean);
 }
 
+function getVideoId(url) {
+  const match = url.match(/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^/]+\/.+|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/|youtube.com\/shorts\/)([^"&?/\s]{11})/i);
+  return match ? match[1] : null;
+}
+
 // Funktion zum Vorladen eines YouTube-Videos
-async function preloadVideo(youtubeURL) {
+async function preloadVideo(youtubeURL, onData) {
+  const videoInfo = await ytdl.getInfo(youtubeURL);
+
   return new Promise((resolve, reject) => {
-    const videoStream = ytdl(youtubeURL, { quality: 'highestvideo' });
-    const audioStream = ytdl(youtubeURL, { quality: 'highestaudio' });
+    const videoId = getVideoId(youtubeURL);
+    if (!videoId) {
+      throw new Error(`Ungültige YouTube-URL: ${youtubeURL}`);
+    }
+
+    const videoDetails = {
+      title: videoInfo.videoDetails.title,
+      lengthSeconds: videoInfo.videoDetails.lengthSeconds,
+      author: videoInfo.videoDetails.author.name,
+      uploadDate: videoInfo.videoDetails.uploadDate,
+      description: videoInfo.videoDetails.description
+    };
+
+    fs.writeFileSync(path.join(outputDir, `${videoId}.json`), JSON.stringify(videoDetails, null, 2));
+    const videoFormat = videoInfo.formats.find(f => f.qualityLabel === '1080p') ||
+        videoInfo.formats.find(f => f.qualityLabel === '720p') ||
+        ytdl.chooseFormat(videoInfo.formats, { quality: 'highestvideo' });
+    const audioFormat = ytdl.chooseFormat(videoInfo.formats, { quality: 'highestaudio' });
+
+
+    const videoStream = ytdl.downloadFromInfo(videoInfo, { format: videoFormat });
+    const audioStream = ytdl.downloadFromInfo(videoInfo, { format: audioFormat });
 
     const ffmpegMerge = spawn('ffmpeg', [
       '-i', 'pipe:0',
@@ -45,10 +109,7 @@ async function preloadVideo(youtubeURL) {
     videoStream.pipe(ffmpegMerge.stdio[0]);
     audioStream.pipe(ffmpegMerge.stdio[1]);
 
-    const buffers = [];
-    ffmpegMerge.stdio[2].on('data', (data) => {
-      buffers.push(data);
-    });
+    ffmpegMerge.stdio[2].on('data', onData);
 
     ffmpegMerge.on('error', (err) => {
       console.error('Fehler bei ffmpeg (Merge):', err);
@@ -57,7 +118,7 @@ async function preloadVideo(youtubeURL) {
 
     ffmpegMerge.on('exit', (code, signal) => {
       if (code === 0) {
-        resolve(Buffer.concat(buffers));
+        resolve();
       } else {
         reject(new Error(`FFmpeg (Merge) wurde mit Code ${code} und Signal ${signal} beendet`));
       }
@@ -65,23 +126,19 @@ async function preloadVideo(youtubeURL) {
   });
 }
 
-// Hauptfunktion zum Streamen der Playlist
-async function streamPlaylist() {
-  const playlist = readPlaylist('playlist.txt');
-  console.log('Starte Streaming der Playlist');
-
-  // Starten Sie den FFmpeg-Prozess, um die FIFO-Datei zu lesen und an den RTMP-Server zu streamen
+// Funktion zum Starten des FFmpeg-Prozesses für das Streamen zur RTMP-URL
+function startRtmpStreaming(inputFifoPath, rtmpUrl) {
   const ffmpegProcess = spawn('ffmpeg', [
-    '-re',
-    '-i', fifoPath,
+    '-i', inputFifoPath,
     '-c:v', 'libx264',
     '-c:a', 'aac',
+    '-b:a', '192k',
     '-vf', 'scale=1920:1080',
-    '-b:v', '7000k',
-    '-crf', '18',
+    '-b:v', '4500k',
+    '-crf', '23',
     '-f', 'flv', 
-    '-maxrate', '8000k', 
-    '-bufsize', '16000k', 
+    '-bufsize', '4500k',
+    '-maxrate', '9000k',
     rtmpUrl
   ]);
 
@@ -96,28 +153,55 @@ async function streamPlaylist() {
       console.error(`FFmpeg (Stream) wurde mit Code ${code} und Signal ${signal} beendet`);
     }
   });
+}
 
+function nextVideo(url) {
+    const ytId = getVideoId(url);
+    
+    setTimeout(() => {
+      const data = JSON.parse(fs.readFileSync(`./data/${ytId}.json`, 'UTF-8'));
+      twitchApi.channels.updateChannelInfo(channelUserId, {title: data.title});
+      console.log('Change Titel: ' + data.title);
+    }, 10000);
+}
+
+// Hauptfunktion zum Streamen der Playlist
+async function streamPlaylist(playlist) {
+  console.log('Starte Streaming der Playlist');
+  // Starte das Streamen zur RTMP-URL
+  startRtmpStreaming(fifoPath, rtmpUrl);
+
+  let i = 0;
   while (true) {
-    for (let i = 0; i < playlist.length; i++) {
-      const url = playlist[i];
-      console.log(`Verarbeite Video ${i + 1} von ${playlist.length}: ${url}`);
+    const url = playlist[i % playlist.length];
+    
+    console.log(`Verarbeite Video ${i + 1} von ${playlist.length}: ${url}`);
 
-      try {
-        // Vorladen des nächsten Videos
-        const videoBuffer = await preloadVideo(url);
+    // Überprüfe, ob der FIFO-Puffer nicht voll ist, bevor das Video vorab geladen wird
+    try {
+      // Vorladen des Videos
+      nextVideo(url);
+      // Schreibe das vorab geladene Video in die FIFO-Datei
+      const writeStream = fs.createWriteStream(fifoPath, { flags: 'a' });
 
-        // Schreibe das vorab geladene Video in die FIFO-Datei
-        const writeStream = fs.createWriteStream(fifoPath, { flags: 'a' });
-        writeStream.write(videoBuffer);
-        writeStream.end();
+      await preloadVideo(url, (data) => {
+        if (!writeStream.write(data)) {
+          writeStream.once('drain', () => {
+          });
+        }
+      });
 
-        console.log(`Video ${i + 1} von ${playlist.length} gestreamt`);
-      } catch (err) {
-        console.error(`Fehler beim Streamen von Video ${i + 1} von ${playlist.length}: ${err.message}`);
+      await videoBuffer.resolve;
+
+      if (++i >= playlist.length) {
+        i = 0;
       }
+    } catch (err) {
+      console.error(`Fehler beim Streamen von Video ${i + 1} von ${playlist.length}: ${err.message}`);
     }
-    console.log('Wiederhole die Playlist');
   }
 }
 
-streamPlaylist();
+// Starte die Playlist-Streaming-Funktion
+const playlist = readPlaylist('playlist.txt');
+streamPlaylist(playlist);
