e5bf0e71354cc0b24e628d9989695fa5073d114b
nofuture2077 <37042983+nofuture2077@users.noreply.github.com>
2024-06-05 12:38:14 +0200

    video offset

--- playlist.js
+++ playlist.js
@@ -60,10 +60,40 @@ function getCurrentVideo(playlist, currentTime) {
     return null;
 }
 
+/**
+ * Find the current video index and the offset in the playlist at a given time.
+ * 
+ * @param {Array} playlist - Array of video objects, each containing a YouTube video ID and length in milliseconds.
+ * @param {number} currentTime - The current time in milliseconds.
+ * @returns {Object} An object containing the index of the current video and the offset in milliseconds.
+ */
+function getCurrentVideoIndexAndOffset(playlist, currentTime) {
+    // Calculate the total duration of the playlist
+    const totalDuration = playlist.reduce((acc, video) => acc + video.lengthMilliSeconds, 0);
+    
+    // Calculate the effective time within the playlist (handle looping)
+    const effectiveTime = currentTime % totalDuration;
+    let elapsedTime = 0;
+
+    // Iterate through the playlist to find the current video and offset
+    for (let index = 0; index < playlist.length; index++) {
+        const video = playlist[index];
+        elapsedTime += video.lengthMilliSeconds;
+        if (effectiveTime < elapsedTime) {
+            const offset = effectiveTime - (elapsedTime - video.lengthMilliSeconds);
+            return [ index, offset ];
+        }
+    }
+
+    // In case something goes wrong, return null
+    return [0, 0];
+}
+
 module.exports = {
     readPlaylist,
     readPlaylistInfo,
     createFFmpegPlaylist,
     getFFMpegPlaylistName,
-    getCurrentVideo
+    getCurrentVideo,
+    getCurrentVideoIndexAndOffset
 }
